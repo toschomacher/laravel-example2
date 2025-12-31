e9bb6392f2339207d5454b1fb420b5c9c7730962
Todor Vasilev <114831156+toschomacher@users.noreply.github.com>
2025-12-31 13:33:56 +0000

    Laracast day 7 job array duplication fix

--- routes/web.php
+++ routes/web.php
@@ -3,38 +3,7 @@
 use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () 
-    {
-        return view('home', ['greeting' => 'Hello']);
-    }
-);
-Route::get('/jobs', function()
-    {
-        return view('jobs', [
-            'jobs' => [
-                [
-                    'id' => '1',
-                    'title' => 'Director',
-                    'salary' => '£50,000.00'
-                ],
-                [
-                    'id' => '2',
-                    'title' => 'Programmer',
-                    'salary' => '£40,000.00'
-                ],
-                [
-                    'id' => '3',
-                    'title' => 'Teacher',
-                    'salary' => '£45,000.00'
-                ]
-            ]
-        ]);
-    }
-);
-
-Route::get('/jobs/{id}', function($id) //when laravel sees something in {} it knows it's a wild card
-    {
-        $jobs = [
+$jobs = [
                 [
                     'id' => '1',
                     'title' => 'Director',
@@ -51,6 +20,20 @@ Route::get('/jobs/{id}', function($id) //when laravel sees something in {} it kn
                     'salary' => '£45,000.00'
                 ]
                 ];
+
+Route::get('/', function () 
+    {
+        return view('home', ['greeting' => 'Hello']);
+    }
+);
+Route::get('/jobs', function() use($jobs){
+        return view('jobs', [
+            'jobs' => $jobs
+        ]);
+    }
+);
+
+Route::get('/jobs/{id}', function($id) use($jobs){ //when laravel sees something in {} it knows it's a wild card
         $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
         //dd($job);    // dd is dump and die, 'dump the id and kill the execution'
         return view('job',['job' => $job]);
