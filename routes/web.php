cdf63f194de14cec3ce076b70a799404fee6ec03
Todor Vasilev <114831156+toschomacher@users.noreply.github.com>
2025-12-31 15:18:26 +0000

    Laracast day 7 complete

--- routes/web.php
+++ routes/web.php
@@ -1,41 +1,22 @@
 <?php
 
-use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
-
-$jobs = [
-                [
-                    'id' => '1',
-                    'title' => 'Director',
-                    'salary' => '£50,000.00'
-                ],
-                [
-                    'id' => '2',
-                    'title' => 'Programmer',
-                    'salary' => '£40,000.00'
-                ],
-                [
-                    'id' => '3',
-                    'title' => 'Teacher',
-                    'salary' => '£45,000.00'
-                ]
-                ];
+use App\Models\Job;
 
 Route::get('/', function () 
     {
         return view('home', ['greeting' => 'Hello']);
     }
 );
-Route::get('/jobs', function() use($jobs){
+Route::get('/jobs', function(){
         return view('jobs', [
-            'jobs' => $jobs
+            'jobs' => Job::allJobs()
         ]);
     }
 );
 
-Route::get('/jobs/{id}', function($id) use($jobs){ //when laravel sees something in {} it knows it's a wild card
-        $job = Arr::first($jobs, fn($job) => $job['id'] == $id);
-        //dd($job);    // dd is dump and die, 'dump the id and kill the execution'
+Route::get('/jobs/{id}', function($id) { //when laravel sees something in {} it knows it's a wild card
+        $job = Job::findJob($id);
         return view('job',['job' => $job]);
     }
 );
